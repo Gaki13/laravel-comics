b8cd7983f5425d7f7fb5250f70fc2b6d2faff8e5
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-07-07 17:51:11 +0200

    aggiunta footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,77 @@
-<footer></footer>
+<footer>
+    {{-- sezione superiore del footer  --}}
+   <div class="footer-top">
+        <div class="container container-footer-middle">
+            <!-- lista di links  -->
+            <div class="contact-lists">
+                <div class="col">
+                    <ul class="col-ul">
+                        <h2 class="col-ul-title">DC COMICS</h2>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <ul class="col-ul">
+                        <h2 class="col-ul-title">SHOP</h2>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibiles</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul class="col-ul">
+                        <h2 class="col-ul-title">DC</h2>
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy (New)</li>
+                        <li>Add Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul class="col-ul">
+                        <h2 class="col-ul-title">SITES</h2>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- parte con il logo dc -->
+            <div class="logo-big">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="">
+            </div>
+        </div>
+   </div>
+   {{-- sezione inferiore del footer  --}}
+   <div class="bottom-footer">
+        <div class="container container-footer-bottom">
+            <!-- sezione per il sign up -->
+            <div class="sign-up">
+                <button class="sign-up-btn">
+                    SIGN-UP NOW!
+                </button>
+            </div>
+            <!-- sezione per i social -->
+            <div class="social">
+                <h2 class="social-title">FOLLOW US</h2>
+                <a href="#"><img src="{{asset('/img/footer-facebook.png')}}" alt="icona di facebook"></a>
+                <a href="#"><img src="{{asset('/img/footer-twitter.png')}}" alt="icona di twitter"></a>
+                <a href="#"><img src="{{asset('/img/footer-youtube.png')}}" alt="icona di youtube"></a>
+                <a href="#"><img src="{{asset('/img/footer-pinterest.png')}}" alt="icona di pinterest"></a>
+                <a href="#"><img src="{{asset('/img/footer-periscope.png')}}" alt="icona di periscope"></a>
+            </div>
+        </div>
+   </div>
+</footer>
