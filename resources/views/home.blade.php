d16b5ac4dcc9382ea2c3a58147931e18d6d6a516
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-07-07 18:15:16 +0200

    aggiunta sezione principale del main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,16 @@
 @endsection
 
 @section('page-content')
-    
+<div class="container">
+        <ul class="comics-list">
+            @foreach ($comics as $comic)
+                <li class="card">
+                        <img src="{{$comic["thumb"]}}" alt="copertina del comics">
+                        <h3>{{$comic["title"]}}</h3>
+                </li>
+            @endforeach
+        </ul>
+       
+    <button class="main-btn"><h3>LOAD MORE</h3></button>   
+</div>
 @endsection
